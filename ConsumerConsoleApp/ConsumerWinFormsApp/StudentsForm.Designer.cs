084ea51558ea23f2e584d226242320a239161618
Foyzul Karim <foyzulkarim@gmail.com>
2016-12-01 07:28:48 +0600

    student entry code complete

--- ConsumerConsoleApp/ConsumerWinFormsApp/StudentsForm.Designer.cs
+++ ConsumerConsoleApp/ConsumerWinFormsApp/StudentsForm.Designer.cs
@@ -28,26 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.studentTabControl = new System.Windows.Forms.TabControl();
             this.studentEntryTabPage = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
             this.studentListTabPage = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
+            this.studentTabControl.SuspendLayout();
+            this.studentEntryTabPage.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // studentTabControl
             // 
-            this.tabControl1.Controls.Add(this.studentEntryTabPage);
-            this.tabControl1.Controls.Add(this.studentListTabPage);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.ItemSize = new System.Drawing.Size(75, 40);
-            this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(622, 553);
-            this.tabControl1.TabIndex = 0;
+            this.studentTabControl.Controls.Add(this.studentEntryTabPage);
+            this.studentTabControl.Controls.Add(this.studentListTabPage);
+            this.studentTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.studentTabControl.ItemSize = new System.Drawing.Size(75, 40);
+            this.studentTabControl.Location = new System.Drawing.Point(0, 0);
+            this.studentTabControl.Name = "studentTabControl";
+            this.studentTabControl.SelectedIndex = 0;
+            this.studentTabControl.Size = new System.Drawing.Size(622, 553);
+            this.studentTabControl.TabIndex = 0;
             // 
             // studentEntryTabPage
             // 
+            this.studentEntryTabPage.Controls.Add(this.tableLayoutPanel1);
             this.studentEntryTabPage.Location = new System.Drawing.Point(4, 44);
             this.studentEntryTabPage.Name = "studentEntryTabPage";
             this.studentEntryTabPage.Padding = new System.Windows.Forms.Padding(3);
@@ -55,6 +67,92 @@
             this.studentEntryTabPage.TabIndex = 0;
             this.studentEntryTabPage.Text = "New Student";
             this.studentEntryTabPage.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.72368F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 79.27632F));
+            this.tableLayoutPanel1.Controls.Add(this.phoneTextBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.nameTextBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 162F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(608, 499);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(128, 53);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(387, 22);
+            this.phoneTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label2.Location = new System.Drawing.Point(73, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Phone";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Location = new System.Drawing.Point(77, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(128, 23);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(387, 22);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.saveButton);
+            this.flowLayoutPanel1.Controls.Add(this.clearButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(128, 83);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(477, 156);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(3, 3);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(100, 30);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(109, 3);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 30);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
             // studentListTabPage
             // 
@@ -71,19 +169,31 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(622, 553);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.studentTabControl);
             this.Name = "StudentsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "StudentsForm";
-            this.tabControl1.ResumeLayout(false);
+            this.studentTabControl.ResumeLayout(false);
+            this.studentEntryTabPage.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl studentTabControl;
         private System.Windows.Forms.TabPage studentEntryTabPage;
         private System.Windows.Forms.TabPage studentListTabPage;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button clearButton;
     }
 }
--- ConsumerConsoleApp/ConsumerWinFormsApp/StudentsForm.cs
+++ ConsumerConsoleApp/ConsumerWinFormsApp/StudentsForm.cs
@@ -7,14 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ApplicationLibrary;
+using ApplicationLibrary.Models.Students;
+using Commons.Repository;
+using Commons.Service;
 
 namespace ConsumerWinFormsApp
 {
     public partial class StudentsForm : Form
     {
+        
         public StudentsForm()
         {
             InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            BaseRepository<Student> repository=new BaseRepository<Student>(new BusinessDbContext());
+            BaseService<Student,StudentRequestModel,StudentViewModel> service  =new BaseService<Student, StudentRequestModel, StudentViewModel>(repository);
+            Student student=new Student();
+            student.Name = nameTextBox.Text;
+            student.Phone = phoneTextBox.Text;
+            student.Id=Guid.NewGuid().ToString();
+            student.Created=DateTime.Now;
+            student.Modified=DateTime.Now;
+            student.CreatedBy = "me";
+            student.ModifiedBy = "me";
+            service.Add(student);
+            MessageBox.Show("Saved");
+            ClearForm();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+        private void ClearForm()
+        {
+            nameTextBox.Clear();
+            phoneTextBox.Clear();
         }
     }
 }
