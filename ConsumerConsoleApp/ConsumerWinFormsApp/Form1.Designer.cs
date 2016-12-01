91f5ae39a24d6abcf84840ce80973338ac7c3f21
Foyzul Karim <foyzulkarim@gmail.com>
2016-12-01 07:12:17 +0600

    started student form

---
+++ ConsumerConsoleApp/ConsumerWinFormsApp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ConsumerWinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studentsButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // studentsButton
+            // 
+            this.studentsButton.Location = new System.Drawing.Point(28, 25);
+            this.studentsButton.Name = "studentsButton";
+            this.studentsButton.Size = new System.Drawing.Size(168, 73);
+            this.studentsButton.TabIndex = 0;
+            this.studentsButton.Text = "Students";
+            this.studentsButton.UseVisualStyleBackColor = true;
+            this.studentsButton.Click += new System.EventHandler(this.studentsButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(235, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 73);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Departments";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(443, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(168, 73);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Course";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(136, 422);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(386, 78);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 512);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.studentsButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button studentsButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
---
+++ ConsumerConsoleApp/ConsumerWinFormsApp/StudentsForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ConsumerWinFormsApp
+{
+    partial class StudentsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.studentEntryTabPage = new System.Windows.Forms.TabPage();
+            this.studentListTabPage = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.studentEntryTabPage);
+            this.tabControl1.Controls.Add(this.studentListTabPage);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.ItemSize = new System.Drawing.Size(75, 40);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(622, 553);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // studentEntryTabPage
+            // 
+            this.studentEntryTabPage.Location = new System.Drawing.Point(4, 44);
+            this.studentEntryTabPage.Name = "studentEntryTabPage";
+            this.studentEntryTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.studentEntryTabPage.Size = new System.Drawing.Size(614, 505);
+            this.studentEntryTabPage.TabIndex = 0;
+            this.studentEntryTabPage.Text = "New Student";
+            this.studentEntryTabPage.UseVisualStyleBackColor = true;
+            // 
+            // studentListTabPage
+            // 
+            this.studentListTabPage.Location = new System.Drawing.Point(4, 44);
+            this.studentListTabPage.Name = "studentListTabPage";
+            this.studentListTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.studentListTabPage.Size = new System.Drawing.Size(614, 505);
+            this.studentListTabPage.TabIndex = 1;
+            this.studentListTabPage.Text = "Students List";
+            this.studentListTabPage.UseVisualStyleBackColor = true;
+            // 
+            // StudentsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 553);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "StudentsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "StudentsForm";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage studentEntryTabPage;
+        private System.Windows.Forms.TabPage studentListTabPage;
+    }
+}
