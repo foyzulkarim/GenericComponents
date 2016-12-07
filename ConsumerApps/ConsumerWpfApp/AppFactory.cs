fd8a45c8ec839bb6c8962f8054f2fccc9f866094
Foyzul Karim <foyzulkarim@gmail.com>
2016-12-07 09:18:07 +0600

    started building wpf datagrid control

---
+++ ConsumerApps/ConsumerWpfApp/AppFactory.cs
@@ -0,0 +1,26 @@
+﻿using ApplicationLibrary;
+using ApplicationLibrary.Models.Departments;
+using ApplicationLibrary.Models.Students;
+using Commons.Model;
+using Commons.Repository;
+using Commons.Service;
+
+namespace ConsumerWpfApp
+{
+    public static class AppFactory
+    {
+        static AppFactory()
+        {
+            StudentService = new BaseService<Student, StudentRequestModel, StudentViewModel>(CreateRepository<Student>());
+            DepartmentService = new BaseService<Department, DepartmentRequestModel, DepartmetnViewModel>(CreateRepository<Department>());
+        }
+
+        public static BaseRepository<T> CreateRepository<T>() where T : Entity
+        {
+            return new BaseRepository<T>(BusinessDbContext.CreateInstance());
+        }
+
+        public static BaseService<Student, StudentRequestModel, StudentViewModel> StudentService { get; set; }
+        public static BaseService<Department, DepartmentRequestModel, DepartmetnViewModel> DepartmentService { get; set; }
+    }
+}
---
+++ ConsumerApps/ConsumerWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ApplicationLibrary.Models.Students;
+
+namespace ConsumerWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadGrid();
+        }
+
+        private async Task LoadGrid()
+        {
+            StudentRequestModel r = new StudentRequestModel();
+            var result = await AppFactory.StudentService.SearchAsync(r);
+            dataGrid.ItemsSource = result.Item1.OfType<object>().ToList().ConvertToViewableDynamicList();
+        }
+    }
+}
